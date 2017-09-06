92825086cf98847bf4965a244ff067bfc80b764e
nachogoca <nacho.gon.car@gmail.com>
2017-09-05 20:24:40 -0500

    fix: change upload image logic, connect to api, get result

---
+++ logic.js
@@ -0,0 +1,54 @@
+function loadImage() {
+    var imageUrl = document.getElementById("url").value;
+    var image = document.getElementById("img");
+
+    image.src = imageUrl;
+}
+
+function handleFileUpload(doc) {
+    return new Promise(function (resolve, reject) {
+        var file = doc.files[0];
+        var fileReader = new FileReader();
+
+        fileReader.onload = function (e) {
+            resolve(fileReader.result);
+        }
+
+        if (file) {
+            try {
+                fileReader.readAsDataURL(file);
+            } catch (error) {
+                console.log(error);
+                reject(error);
+            }
+
+        }
+    })
+}
+
+const app = new Clarifai.App({
+    apiKey: 'a309cbeae1d54537b5956f31bcb385e8'
+});
+
+function analyzeImage() {
+
+    var imageSource = document.getElementById("url").value;
+    app.models.predict("aaa03c23b3724a16a56b629203edc62c", imageSource).then(
+        function (response) {
+            debugger;
+            var resultList = document.getElementById("resultList");
+
+            var concepts = response.outputs[0].data.concepts;
+            for (let i = 0; i < concepts.length; i++) {
+                var li = document.createElement("li");
+                var conceptText = "Concept : " + concepts[i].name + ".  Certainty: " + concepts[i].value;
+                li.appendChild(document.createTextNode(conceptText));
+                resultList.appendChild(li);
+            }
+        },
+        function (err) {
+            console.log(err);
+        }
+    );
+
+}
